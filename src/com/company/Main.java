0be3c440a18850f6c487707fca89063ad3dcebc2
zzn <zzn987@126.com>
2020-10-28 16:48:25 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("111");
+//        System.out.println("111");
+//        String uuid = UUID.randomUUID().toString().replaceAll("-","");
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
         }
 }
