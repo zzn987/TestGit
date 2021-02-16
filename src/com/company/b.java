82777721480fda642b53b5547002b2ee4860a99c
zzn987 <68216043+zzn987@users.noreply.github.com>
2021-02-16 12:45:14 +0800

    Update b.java

--- src/com/company/b.java
+++ src/com/company/b.java
@@ -3,6 +3,6 @@ package com.company;
 public class b {
     public static void main(String[] args) {
         System.out.println(111);
-        System.out.println(123);
+        System.out.println(1230);
     }
 }
